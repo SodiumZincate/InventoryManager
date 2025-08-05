e7a628806257b348d950da37e58fef57cbde3499
Visual Studio <darkthunder248@gmail.com>
2025-08-05 19:34:28 +0545

    Added LogAdapter.java and LogEntry.java

---
+++ app/src/main/java/com/example/inventorymanager/LogAdapter.java
@@ -0,0 +1,57 @@
+package com.example.inventorymanager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+public class LogAdapter extends RecyclerView.Adapter<LogAdapter.LogViewHolder>{
+    private ArrayList<LogEntry> logs;
+
+    public LogAdapter(ArrayList<LogEntry> logs){
+        this.logs = logs;
+    }
+
+    @NonNull
+    @Override
+    public LogViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_log, parent, false);
+        return new LogViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull LogViewHolder holder, int position){
+        LogEntry log = logs.get(position);
+
+        String componentText = "Component: " + log.getComponentName();
+        String takenByText = "Taken By: " + log.getTakenBy();
+        String burrowedDateText = "Burrowed Date: " + log.getBurrowedDate();
+        String returnDateText = "Return Date: " + log.getReturnDate();
+        holder.tvComponentName.setText(componentText);
+        holder.tvTakenBy.setText(takenByText);
+        holder.tvBurrowedDate.setText(burrowedDateText);
+        holder.tvReturnDate.setText(returnDateText);
+    }
+
+    @Override
+    public int getItemCount() {
+        return logs.size();
+    }
+
+    public static class LogViewHolder extends RecyclerView.ViewHolder{
+        TextView tvComponentName, tvTakenBy, tvBurrowedDate, tvReturnDate;
+
+        public LogViewHolder(@NonNull View itemView){
+            super(itemView);
+            tvComponentName = itemView.findViewById(R.id.tvComponentName);
+            tvTakenBy = itemView.findViewById(R.id.tvTakenBy);
+            tvBurrowedDate = itemView.findViewById(R.id.tvBurrowedDate);
+            tvReturnDate = itemView.findViewById(R.id.tvReturnDate);
+        }
+    }
+}
