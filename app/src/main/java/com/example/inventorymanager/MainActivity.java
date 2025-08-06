5d459a5c05ead50b168f35b36db1bf76e78f767b
Visual Studio <darkthunder248@gmail.com>
2025-08-06 23:33:17 +0545

    Changed default to light mode and updated application icon

--- app/src/main/java/com/example/inventorymanager/MainActivity.java
+++ app/src/main/java/com/example/inventorymanager/MainActivity.java
@@ -12,6 +12,7 @@ import com.example.inventorymanager.R;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LiveData;
 import androidx.recyclerview.widget.RecyclerView;
@@ -30,6 +31,8 @@ public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
