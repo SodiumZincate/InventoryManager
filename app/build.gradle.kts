bb26301bbddb48aa7cebb92b512489e43a4223fc
Visual Studio <darkthunder248@gmail.com>
2025-08-06 22:44:49 +0545

    Updated gradle versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
+    implementation(libs.material)
+
 }
